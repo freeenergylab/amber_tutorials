b0d6e6a43ea4ffe1cc8fcf0a32d4a81f964b435e
lipengfei <lipengfei_mail@126.com>
2024-01-05 14:02:13 +0800

    updates Antechamber/organic_molecule

--- Antechamber/organic_molecule/antechamber.sh
+++ Antechamber/organic_molecule/antechamber.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-$AMBERHOME/bin/antechamber -i ejm_31.sdf -fi sdf -o vacuum.mol2 -fo mol2 -at gaff2 -c bcc -an n -nc 0 -rn MOL -dr no -pf y > /dev/null 2>&1
+$AMBERHOME/bin/antechamber -i ejm_31.sdf -fi mdl -o vacuum.mol2 -fo mol2 -at gaff2 -c bcc -an n -nc 0 -rn MOL -dr no -pf y > /dev/null 2>&1
 $AMBERHOME/bin/parmchk2 -i vacuum.mol2 -f mol2 -o vacuum.frcmod -a Y -p $AMBERHOME/dat/leap/parm/gaff2.dat
 grep "ATTN:" vacuum.frcmod

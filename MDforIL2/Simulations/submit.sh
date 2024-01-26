25a1053bc2247d7bbc1ee0aefb8aab891ae4929f
lipengfei <lipengfei_mail@126.com>
2024-01-26 22:14:43 +0800

    updates on MDforIL2/Simulations

--- MDforIL2/Simulations/submit.sh
+++ MDforIL2/Simulations/submit.sh
@@ -11,8 +11,8 @@ echo INFO: AMBERHOME $AMBERHOME
 nproc=32
 export CUDA_VISIBLE_DEVICES=0
 
-mpirun -np $nproc $AMBERHOME/bin/pmemd.MPI -O -i min1.in -p complex.parm7 -c complex.rst7 -ref complex.rst7 -o min1.out -r min1.rst7 -inf min1.info
-mpirun -np $nproc $AMBERHOME/bin/pmemd.MPI -O -i min2.in -p complex.parm7 -c min1.rst7 -ref min1.rst7 -o min2.out -r min2.rst7 -inf min2.info
+mpirun --oversubscribe -np $nproc $AMBERHOME/bin/pmemd.MPI -O -i min1.in -p complex.parm7 -c complex.rst7 -ref complex.rst7 -o min1.out -r min1.rst7 -inf min1.info
+mpirun --oversubscribe -np $nproc $AMBERHOME/bin/pmemd.MPI -O -i min2.in -p complex.parm7 -c min1.rst7 -ref min1.rst7 -o min2.out -r min2.rst7 -inf min2.info
 $AMBERHOME/bin/pmemd.cuda -O -i heat-1.in -p complex.parm7 -c min2.rst7 -ref min2.rst7 -o heat-1.out -r heat-1.rst7 -x heat-1.nc -inf heat-1.info
 $AMBERHOME/bin/pmemd.cuda -O -i press-1.in -p complex.parm7 -c heat-1.rst7 -ref heat-1.rst7 -o press-1.out -r press-1.rst7 -x press-1.nc -inf press-1.info
 $AMBERHOME/bin/pmemd.cuda -O -i heat-2.in -p complex.parm7 -c press-1.rst7 -ref press-1.rst7 -o heat-2.out -r heat-2.rst7 -x heat-2.nc -inf heat-2.info

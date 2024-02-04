c7c8b1e4e62ca175eed2a0025c8021e24f70d2df
lipengfei <lipengfei_mail@126.com>
2024-02-04 16:32:34 +0800

    updates on MDforDNAzyme/Simulations/equil

--- MDforDNAzyme/Simulations/equil/submit_gpu_jobs.sh
+++ MDforDNAzyme/Simulations/equil/submit_gpu_jobs.sh
@@ -8,7 +8,6 @@ echo INFO: WORKDIR $PWD
 echo INFO: HOSTNAME `hostname`
 echo INFO: AMBERHOME $AMBERHOME
 
-EXE=$AMBERHOME/bin/pmemd.cuda
 PRMTOP=../../systemPreparation/tleap/complex.parm7
 
 export CUDA_VISIBLE_DEVICES=1
@@ -16,36 +15,36 @@ export CUDA_VISIBLE_DEVICES=1
 NEW=min_ntr_n
 OLD=$NEW.rst7
 NEW=md_nvt_ntr
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_npt_ntr_01
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_npt_ntr_02
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_nvt_red_01
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_nvt_red_02
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_nvt_red_03
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_nvt_red_04
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_nvt_red_05
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=md_nvt_red_06
-$EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc
+$AMBERHOME/bin/pmemd.cuda -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -x $NEW.nc -info $NEW.info
--- MDforDNAzyme/Simulations/equil/submit_jobs.sh
+++ MDforDNAzyme/Simulations/equil/submit_jobs.sh
@@ -9,21 +9,19 @@ echo INFO: HOSTNAME `hostname`
 echo INFO: AMBERHOME $AMBERHOME
 
 nproc=32
-DO_PARALLEL=mpirun --oversubscribe -np $nproc
-EXE=$AMBERHOME/bin/pmemd.MPI
 PRMTOP=../../systemPreparation/tleap/complex.parm7
 INPCRD=../../systemPreparation/tleap/complex.rst7
 
 OLD=$INPCRD
 NEW=min_ntr_h
-$DO_PARALLEL $EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD
+mpirun --oversubscribe -np $nproc $AMBERHOME/bin/pmemd.MPI -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=min_ntr_l
-$DO_PARALLEL $EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD
+mpirun --oversubscribe -np $nproc $AMBERHOME/bin/pmemd.MPI -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -info $NEW.info
 
 OLD=$NEW.rst7
 NEW=min_ntr_n
-$DO_PARALLEL $EXE -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD
+mpirun --oversubscribe -np $nproc $AMBERHOME/bin/pmemd.MPI -O -i ${NEW}.in -o $NEW.out -p $PRMTOP -c $OLD -r $NEW.rst7 -ref $OLD -info $NEW.info
 
 source ./submit_gpu_jobs.sh
